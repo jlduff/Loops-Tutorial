a9b0fb79eb66d955802c3029cd50f788cef54011
LON DUFF <johncduffman@gmail.com>
2020-10-09 23:49:02 -0400

    added file

---
+++ list_loops.py
@@ -0,0 +1,29 @@
+#songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+#print(songs[0:2])
+
+#songs[0] = "Back to Black"
+
+#print (songs)
+
+#songs.append("Pursuit of Happiness")
+#songs.extend(["Ichidaiji", "Big Yellow Taxi"])
+#Joni Mitchell is a queen
+#songs.insert(0, "Fast Car") 
+#Tracy Chapman is a queen
+
+#songs.remove("Back to Black")
+#songs.pop(1)
+#songs.clear()
+#del songs[2]
+
+#for song in songs:
+    #print(song)
+
+animals = ["Fox", "Lynx", "Whale"]
+animals.append("Terrier")
+print(animals[2])
+del animals[0]
+
+for animal in animals: 
+    print[animal]
